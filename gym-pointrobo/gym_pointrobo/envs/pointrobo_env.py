92a8a43ec3865d919f6fba3b3be4d7f9077ae200
KatharinaHermann <hermann-katharina@gmx.de>
2020-05-25 21:25:18 +0200

    Debugged and Improved Environment for a one pixel robot

--- gym-pointrobo/gym_pointrobo/envs/pointrobo_env.py
+++ gym-pointrobo/gym_pointrobo/envs/pointrobo_env.py
@@ -3,11 +3,16 @@ from gym import error, spaces, utils
 from gym.utils import seeding
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.ndimage as ndimage
+import sys
+sys.path.insert(1, 'C:/Users/Katharina Hermann/Documents/UniMaster/2.Semester/ADLR/Project/GITHUB/project/lib')
+#sys.path.append("C:/Users/Katharina Hermann/Documents/UniMaster/2.Semester/ADLR/Project/GITHUB/project/lib")
+from random_workspace import random_workspace 
 
 class PointroboEnv(gym.Env):
-  #metadata = {'render.modes': ['human']}
+  metadata = {'render.modes': ['human']}
 
-  def __init__(self, grid_size, start_pos, goal_pos, workspace,MAX_EPISODE_STEPS):
+  def __init__(self, grid_size, start_pos, goal_pos, workspace, MAX_EPISODE_STEPS):
      super(PointroboEnv, self).__init__()
 
      # Size of the 1D-grid
@@ -18,6 +23,7 @@ class PointroboEnv(gym.Env):
      self.goal_pos = goal_pos
      self.workspace= workspace
      self.MAX_EPISODE_STEPS=MAX_EPISODE_STEPS
+     self.current_step=1
 
      # Define action and observation space
      # They must be gym.spaces objects
@@ -26,43 +32,44 @@ class PointroboEnv(gym.Env):
      self.action_space = spaces.Box(low=0, high=10, shape=
                     (2, 1), dtype=np.uint8)
     
-     # The observation will be the coordinate of the agent
-    # this can be described by Box space
+     # The observation will be the coordinate of the agent *****SHOULD WE OBSERVE MORE?**********
+     #this can be described by Box space
      self.observation_space = spaces.Box(low=0, high=self.grid_size,
                                         shape=(1,), dtype=np.float32)
 
 
   def step(self, action):
+      
       self._take_action(action) 
 
-      self.agent_pos[0] = np.clip(self.agent_pos[0], 0, self.grid_size)
-      self.agent_pos[1] = np.clip(self.agent_pos[1], 0, self.grid_size)
+      self.agent_pos[0,0] = np.clip(self.agent_pos[0,0], 0, self.grid_size)
+      self.agent_pos[0,1] = np.clip(self.agent_pos[0,1], 0, self.grid_size)
 
       self.current_step += 1
-    
+      done = 0
       #Episode stop
       #Have we reached the goal?
-      if self.agent_pos == self.goal_pos
+      if (self.agent_pos[0,0] == self.goal_pos[0,0]) and (self.agent_pos[0,1] == self.goal_pos[0,1]):
         done = 1
         #Have we reached the maximum episode steps?
-        elif self.current_step==self.MAX_EPISODE_STEPS:
-            done = 1
-            #Have we hit an obstacle?
-            elif self.collision_check(self.agent_pos)==1:
-                done = 1
+      elif self.current_step==self.MAX_EPISODE_STEPS:
+        done = 1
+        #Have we hit an obstacle?
+      elif self.collision_check()==1:
+        done = 1
 
       #Goal reached: Reward=1; Obstacle Hit: Reward=-1; Step made=-0.01
-      if self.agent_pos == self.goal_pos:
+      if (self.agent_pos[0,0] == self.goal_pos[0,0]) and (self.agent_pos[0,1] == self.goal_pos[0,1]):
         reward = 1
         #Have we hit an obstacle?
-        elif self.collision_check(self.agent_pos)==1:
-            reward=-1
-            #We made another step
-            else: 
-             reward=-0.01       
+      elif self.collision_check()==1:
+        reward=-1
+        #We made another step
+      else: 
+        reward=-0.01       
 
       return np.array([self.agent_pos]).astype(np.float32), reward, done, {}
-    ...
+
   def reset(self):
     self.agent_pos = self.start_pos
     # here we convert to float32 to make it more general (in case we want to use continuous actions)
@@ -72,27 +79,93 @@ class PointroboEnv(gym.Env):
     # Render the environment to the screen
     if mode != 'console':
       raise NotImplementedError()
-    # agent is represented as a cross, rest as a dot
-
+    
+    # represend environment
     self.workspace[self.start_pos[0,0],self.start_pos[0,1]]=2
     self.workspace[self.goal_pos[0,0],self.goal_pos[0,1]]=4
-    self.workspace[self.agent_pos[0],self.agent_pos[0]]]=3
+    self.workspace[self.agent_pos[0,0],self.agent_pos[0,1]]=3
 
     fig = plt.figure()
-    ax = fig.add_subplot(111)
-    cax = ax.matshow(self.workspace_sample)
+    ax = fig.add_subplot(221)
+    cax = ax.matshow(self.workspace)
     fig.colorbar(cax)
+
+    dist_img = ndimage.distance_transform_edt(-self.workspace + 1)  # Excpects blocks as 0 and free space as 1
+
+    dm = fig.add_subplot(212)
+    cdm=dm.imshow(dist_img, cmap='Reds')
+    cdm=dm.imshow(self.workspace, cmap='Blues', alpha=0.3)
+    plt.show()
 
   def _take_action(self, action):
     action_x = action[0]
     action_y = action[1]
     t=1
 
-    self.agent_pos[1] += action_x*t
-    self.agent_pos[0] += action_y*t
+    self.agent_pos[0,1] += action_x*t
+    self.agent_pos[0,0] += action_y*t
 
-  def collision_check(self, self.agent_pos):
-      collision = 0
-      if self.workspace[self.agent_pos[0],self.agent_pos[1]]==1:
+  def collision_check(self):
+    collision = 0
+
+    #Treat boundaries as obstacles
+    padding_workspace=np.ones((34,34))
+    padding_workspace[1:33,1:33]=self.workspace
+
+    #Create distance map
+    dist_img = ndimage.distance_transform_edt(-padding_workspace + 1)  # Excpects blocks as 0 and free space as 1
+    pixel_size=1 #10/32
+    #Compute distance to the nearest obstacle at the center
+    dist_fun = image_interpolation(img=dist_img, pixel_size=pixel_size)
+    x=np.array([self.agent_pos[0,0],self.agent_pos[0,1]])
+    nearest_dist = dist_fun(x=x)
+    print("Distance to the nearest obstacle at the center: ",
+        nearest_dist)
+      
+    if nearest_dist <= 2:
         collision=1
-      return collision
+        print("Collision is: ",
+        collision)
+    return collision
+
+def image_interpolation(*, img, pixel_size=1, order=1, mode='nearest'):
+
+  factor = 1 / pixel_size
+
+  def interp_fun(x):
+    x2 = x.copy()
+
+    if x2.ndim == 1:
+      x2 = x2[np.newaxis, :]
+      # Transform physical coordinates to image coordinates 
+      #x2 *= factor
+      #x2 -= 0.5
+
+    return ndimage.map_coordinates(input=img, coordinates=x2.T, order=order, mode=mode).T
+
+  return interp_fun
+
+
+
+#***************TEST THE ENVIRONMENT******************************
+grid_size=32
+start,goal,workspace=random_workspace(grid_size, 10, 5)
+env = PointroboEnv(grid_size=grid_size, start_pos=start, goal_pos=goal, workspace=workspace, MAX_EPISODE_STEPS=30)
+obs = env.reset()
+env.render()
+
+action=[1,1]
+# Hardcoded best agent: always go left!
+n_steps = 20
+for step in range(n_steps):
+  print("Step {}".format(step + 1))
+  obs, reward, done, info = env.step(action)
+  print('obs=', obs, 'reward=', reward, 'done=', done)
+  env.render()
+  if done:
+    print("reward=", reward)
+    if reward==1:
+      print ("Goal reached!")
+    elif reward==-1:
+      print ("OOOOpssss you crashed!!")
+    break
--- lib/random_workspace.py
+++ lib/random_workspace.py
@@ -77,45 +77,44 @@ def random_workspace(grid_size, num_obj_max, obj_size_avg):
 
 
 #################### Random workspace generation TEST#############
-start,goal,workspace_sample=random_workspace(32, 10, 5)
+if __name__=='__main__':
+    start,goal,workspace_sample=random_workspace(32, 10, 5)
 
-workspace_sample[start[0,0],start[0,1]]=2
-workspace_sample[goal[0,0],goal[0,1]]=3
+    workspace_sample[start[0,0],start[0,1]]=2
+    workspace_sample[goal[0,0],goal[0,1]]=3
 
-fig = plt.figure()
-ws = fig.add_subplot(221)
-cws = ws.matshow(workspace_sample)
-fig.colorbar(cws)
+    fig = plt.figure()
+    ws = fig.add_subplot(221)
+    cws = ws.matshow(workspace_sample)
+    fig.colorbar(cws)
 
 
-dist_img = ndimage.distance_transform_edt(-workspace_sample + 1)  # Excpects blocks as 0 and free space as 1
+    dist_img = ndimage.distance_transform_edt(-workspace_sample + 1)  # Excpects blocks as 0 and free space as 1
 
-dm = fig.add_subplot(222)
-cdm=dm.imshow(dist_img, cmap='Reds')
-cdm=dm.imshow(workspace_sample, cmap='Blues', alpha=0.3)
-plt.show()
+    dm = fig.add_subplot(222)
+    cdm=dm.imshow(dist_img, cmap='Reds')
+    cdm=dm.imshow(workspace_sample, cmap='Blues', alpha=0.3)
+    plt.show()
 
-def image_interpolation(*, img, pixel_size=1, order=1, mode='nearest'):
+    def image_interpolation(*, img, pixel_size=1, order=1, mode='nearest'):
 
-    factor = 1 / pixel_size
+        factor = 1 / pixel_size
 
-    def interp_fun(x):
-        x2 = x.copy()
+        def interp_fun(x):
+            x2 = x.copy()
 
-        if x2.ndim == 1:
-            x2 = x2[np.newaxis, :]
-        # Transform physical coordinates to image coordinates 
-        #x2 *= factor
-        #x2 -= 0.5
+            if x2.ndim == 1:
+                x2 = x2[np.newaxis, :]
+            # Transform physical coordinates to image coordinates 
+            #x2 *= factor
+            #x2 -= 0.5
 
-        return ndimage.map_coordinates(input=img, coordinates=x2.T, order=order, mode=mode).T
+            return ndimage.map_coordinates(input=img, coordinates=x2.T, order=order, mode=mode).T
 
-    return interp_fun
+        return interp_fun
 
-pixel_size=1 #10/32
+    pixel_size=1 #10/32
 
-dist_fun = image_interpolation(img=dist_img, pixel_size=pixel_size)
-print("Distance to the nearest obstacle at the center: ",
-      dist_fun(x=start))
-print("Distance to the nearest obstacle for 10 random points: ",
-      dist_fun(x=np.random.uniform(low=1, high=9, size=(10, 2))))
+    dist_fun = image_interpolation(img=dist_img, pixel_size=pixel_size)
+    print("Distance to the nearest obstacle at the center: ",
+        dist_fun(x=start))
