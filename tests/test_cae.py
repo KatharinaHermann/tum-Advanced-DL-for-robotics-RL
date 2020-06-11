56a2e8e0880a4a6cd27b38b3ac10a21e18dbd845
Ferenc Török <ferike.trk@gmail.com>
2020-06-11 22:32:15 +0200

    full cae testing is ready

--- tests/test_cae.py
+++ tests/test_cae.py
@@ -1,8 +1,14 @@
-import numpy as np
 import tensorflow as tf
+import tensorflow.keras.optimizers as opt
+import numpy as np
 import os
+import matplotlib.pyplot as plt
 
+from tensorflow.data import Dataset
+from tensorflow.keras.losses import BinaryCrossentropy
 from hwr.cae.cae import CAE
+from hwr.cae.cae_trainer import CAEtrainer, weighted_cross_entropy
+from hwr.random_workspace import visualize_workspace
 
 
 def test_weight_loading():
@@ -17,8 +23,63 @@ def test_weight_loading():
         k.trainable = False
 
 
+def test_autoencoder_training():
+
+    parser = CAEtrainer.get_arguments()
+    args = parser.parse_args()
+
+    args.num_workspaces = 10
+    args.epochs = 10
+    args.batch_size = 2
+    if os.listdir(args.workspace_dir) == 0:
+        args.gen_workspace = True
+    
+    input_shape = (args.grid_size, args.grid_size)
+    model = CAE(args.pooling, args.latent_dim, input_shape, args.conv_filters)
+    optimizer = opt.Adam(learning_rate=args.learning_rate,
+                            beta_1=0.9,
+                            beta_2=0.999,
+                            epsilon=1e-7)
+    print('optimizer: {}'.format(optimizer))
+
+    # loss function. Calculating the positive weights for it:
+    mean_obj_num = (args.num_obj_max + 1) / 2
+    ratio = args.grid_size ** 2 / (mean_obj_num * (args.obj_size_avg ** 2))
+    beta = ratio
+    loss_func = weighted_cross_entropy(beta=beta)
+    print('Loss function: WCE with beta: {}'.format(beta))
+
+    trainer = CAEtrainer(CAE=model,
+                            optimizer=optimizer,
+                            loss_func=loss_func,
+                            args=args)
+
+    trainer()
+
+    # Plot results on an unseen workspace: #
+
+    fig = plt.figure(num=1, figsize=(10, 5))
+    plt.plot(trainer._train_losses)
+    plt.plot(trainer._val_losses)
+
+    # check out the model:
+
+    path = os.path.join('../workspaces/', ('ws_' + str(args.num_workspaces - 1) + '.csv'))
+    x = np.expand_dims(np.loadtxt(path), axis=2).astype('float32')
+    x = np.expand_dims(x, axis=0)
+    x = tf.convert_to_tensor(x)
+
+    x_hat = tf.cast(trainer._CAE(x) >= 0.5, tf.float32)
+
+    fig2 = visualize_workspace(x.numpy()[0, :, :, 0], fignum=2)
+    fig3 = visualize_workspace(x_hat.numpy()[0, :, :, 0], fignum=3)
+
+    plt.show()
+
+
 if __name__ == '__main__':
 
     test_weight_loading()
+    test_autoencoder_training()
     print('All tests have run successfully!')
 
